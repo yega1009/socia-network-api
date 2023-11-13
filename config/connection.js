33936deb6b7c7a5385f3fa82a28a3dcfc0b2bc53
Yega Cho <yega1009@gmail.com>
2023-11-13 18:04:50 -0500

    completed code for index.js that establishes connection to mongoDB and starts the server on a specific port

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-const connectionString = 
-  process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB';
-
-connect(connectionString);
-
-module.exports = connection;
