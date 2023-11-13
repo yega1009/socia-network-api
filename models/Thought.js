d1fec121f5f842fd369b9e33f5087e8be6cf04f8
Yega Cho <yega1009@gmail.com>
2023-11-13 18:32:59 -0500

    defined the 'Thought' mongoose schema and model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => new Date(timestamp).toLocaleString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+}, {
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
