e5361d60176f5921f6f3d9d2b1b62f43af7d008a
Yega Cho <yega1009@gmail.com>
2023-11-14 01:41:51 -0500

    defined all the necessary routes and endpoints for the user  model

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../controllers/userController");
+
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
