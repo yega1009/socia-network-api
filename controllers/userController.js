aba67ea4d7d2836bc98eef7c9b5e0f33a7eefb36
Yega Cho <yega1009@gmail.com>
2023-11-14 00:18:05 -0500

    added CRUD operations for the User model

---
+++ controllers/userController.js
@@ -0,0 +1,106 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all users
+  async getAllUsers(req, res) {
+    try {
+      const users = await User.find().populate("thoughts").populate("friends");
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Get a single user by ID
+  async getUserById(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .populate("thoughts")
+        .populate("friends");
+      // const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  // Update a user by ID
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      //   const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  // Delete a user by ID and remove their thoughts
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      await Thought.deleteMany({ username: user.username });
+      res.json({ message: "User and their thoughts deleted!" });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  // Add a friend to a user's friend list
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  // Remove a friend from a user's friend list
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+};
