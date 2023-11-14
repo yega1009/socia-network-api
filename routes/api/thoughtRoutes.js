24fe2994312f1eb2b49ef0dad12aff124037f078
Yega Cho <yega1009@gmail.com>
2023-11-14 15:48:45 -0500

    added comments for routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,10 +7,15 @@ const {
   deleteThought,
   addReaction,
   deleteReaction
-} = require('../controllers/thoughtController');
+} = require('../../controllers/thoughtController');
 
+// Routes for getting all thoughts and adding a new thought
 router.route('/').get(getAllThoughts).post(addThought);
+
+// Routes for getting, updating, and deleting a single thought by ID
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// Routes for adding a reaction to a thought and deleting a reaction
 router.route('/:thoughtId/reactions').post(addReaction);
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,10 +7,15 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-} = require("../controllers/userController");
+} = require("../../controllers/userController");
 
+// Routes for getting all users and creating a new user
 router.route("/").get(getAllUsers).post(createUser);
+
+// Routes for getting, updating, and deleting a single user by ID
 router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Routes for adding and removing a friend to/from a user's friend list
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
